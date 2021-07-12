32547bbe63e094a9332a93f33479c4b391d817be
Caprinus <rob.cabrerag@alumnos.duoc.cl>
2021-07-12 19:15:27 -0400

    commit bd
    cargamos models.py

--- Evaluacion-3-CA/GrupoZero/core/admin.py
+++ Evaluacion-3-CA/GrupoZero/core/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import  artista, obra
 
 # Register your models here.
+admin.site.register(artista)
+admin.site.register(obra)
--- Evaluacion-3-CA/GrupoZero/core/models.py
+++ Evaluacion-3-CA/GrupoZero/core/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class artista (models.Model):
+     idartista = models.IntegerField(primary_key=True, verbose_name="Id Artista")
+     nombreartista = models.CharField(max_length=50,verbose_name="Nombre Artista")
+     def __str__(self):
+         return self.nombreartista
+
+class obra (models.Model):
+     idObra = models.IntegerField(primary_key=True, verbose_name="Id Obra")
+     detalle = models.CharField(max_length=50,verbose_name="Detalle")
+     valor = models.CharField(max_length=50,verbose_name="Valor")
+     categorias = models.CharField(max_length=50,verbose_name="Categoria")
+     artista = models.ForeignKey(artista, on_delete= models.CASCADE)
+     def __str__(self):
+         return self.detalle
