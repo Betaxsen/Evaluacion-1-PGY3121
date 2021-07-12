67f390fe4b65c452360a126decfb93204d6b07a3
Caprinus <rob.cabrerag@alumnos.duoc.cl>
2021-07-12 19:40:10 -0400

    commit bd2
    con imagen incluida :D

--- Evaluacion-3-CA/GrupoZero/core/models.py
+++ Evaluacion-3-CA/GrupoZero/core/models.py
@@ -13,5 +13,6 @@ class obra (models.Model):
      valor = models.CharField(max_length=50,verbose_name="Valor")
      categorias = models.CharField(max_length=50,verbose_name="Categoria")
      artista = models.ForeignKey(artista, on_delete= models.CASCADE)
+     imagen = models.ImageField(upload_to='core/static/core/img',default=None, blank=True, null=True, verbose_name="Imagen")
      def __str__(self):
          return self.detalle
